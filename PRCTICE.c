d73f495570565283d62289d6353ddf1b91d35206
Sridhar-reddi <92910275+Sridhar-reddi@users.noreply.github.com>
2021-10-29 12:23:03 +0530

    Add files via upload

---
+++ PRCTICE.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	//variabe declaration
+	int age;
+	float cgpa;
+	char initial;
+	
+	//initialization
+	age=20;
+	cgpa=7.20;
+	initial='R';
+	
+	//output
+	printf("%d\n",age);
+	printf("%f\n",cgpa);
+	printf("%c\n",initial);
+}
