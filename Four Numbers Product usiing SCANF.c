875d4efdd7046180c484ff5f3e423b47ee6ab4b1
Sridhar-reddi <92910275+Sridhar-reddi@users.noreply.github.com>
2021-11-10 22:15:07 -0800

    Add files via upload

---
+++ Four Numbers Product usiing SCANF.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main ()
+{
+	int a,b,c,d;
+	printf("Enter four numbers");
+	scanf("%d%d%d%d",&a, &b, &c, &d);
+	/*printf("enter 1st number");
+	scanf("%d", &a);
+	priintf("enter 2nd number");
+	scanf("%d", &b);
+	printf("enter 3rd number");
+	scanf("%d", &c);
+	printf("enter 4th number");
+	scanf("%d", &d);*/
+	printf("%d",a*b*c*d);
+	
+}
